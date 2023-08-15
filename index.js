f3670e41317f5039c3f087e66786635e27589fa2
Azhar Islam <azharislam21@gmail.com>
2023-08-15 15:03:22 +0530

    test 1st commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Sub");
+
+let x = 20;
+if(x===20){
+    console.log("matched");
+}
+
+for(i=0;i<10;i++){
+    console.log(i);
+}
+
+const arr=[2,5,2,4,8,6,9];
+
+console.log(arr);
+console.log(arr[0]);
+
+const app = require('./app')
+console.log(app.x);
